87aadca7e2e858ddda669b2641887494c4ff2ea8
MEHDI ZAMITTI <121120626+mehdizamm@users.noreply.github.com>
2023-09-06 20:29:27 +0200

    firstsave

--- frontend/src/components/General/Navbar.jsx
+++ frontend/src/components/General/Navbar.jsx
@@ -5,36 +5,37 @@ import { NavLink } from "react-router-dom"
 function Navbar() {
   return (
     <>
-      {/* TEMPORAIRE  */}
-      <nav className="navTemporaire">
-        <ul>
-          <li>
-            <NavLink to="/" exact activeClassName="active">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/profil" activeClassName="active">
-              Profil
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/radar" activeClassName="active">
-              Radar
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/alert" activeClassName="active">
-              Alert
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/ia" activeClassName="active">
-              AI
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      {
+        <nav className="navTemporaire">
+          <ul>
+            <li>
+              <NavLink to="/" exact activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/profil" activeClassName="active">
+                Profil
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/radar" activeClassName="active">
+                Radar
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/alert" activeClassName="active">
+                Alert
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/ia" activeClassName="active">
+                AI
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      }
     </>
   )
 }
