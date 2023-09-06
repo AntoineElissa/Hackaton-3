c16341482a377a56521ecfb759fde08d0c881659
Johanna Hilaire <johannahilairepro@gmail.com>
2023-09-06 20:03:23 +0200

    first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import "./styles/Home.scss"
+import Marquee from "../components/Marquee/Marquee.jsx"
 
 function Home() {
   return (
     <div className="home">
-      <div className="home__news"></div>
+      <div className="home__news">
+        <Marquee />
+      </div>
       <div className="home__globals"></div>
       <div className="home__widgets"></div>
     </div>
