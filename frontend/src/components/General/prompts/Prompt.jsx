2c534621a862e04596765524aa39663c067b53ae
Antoine Elissagaray <antoine.elissagaray@orange.fr>
2023-09-06 21:15:53 +0200

    page IA ok

---
+++ frontend/src/components/General/prompts/Prompt.jsx
@@ -0,0 +1,66 @@
+import "./Prompt.scss"
+import { styled } from "styled-components"
+
+function Prompt({ type = "send", data, pic }) {
+  return (
+    <>
+      <StyledImg type={type}>
+        {pic && <img src={pic} alt="img-profil" />}
+      </StyledImg>
+      <PromptStyled type={type}>
+        <PromptBulle type={type} className="prompt__bulle">
+          {data.text}
+
+          <p> {data.time} </p>
+        </PromptBulle>
+      </PromptStyled>
+    </>
+  )
+}
+
+export default Prompt
+
+const PromptStyled = styled.div`
+  width: 100%;
+  min-height: 80px;
+  display: flex;
+
+  ${(props) =>
+    props.type === "send"
+      ? "justify-content: flex-start"
+      : "justify-content: flex-end"}
+`
+
+const PromptBulle = styled.div`
+  width: 80%;
+  height: 100%;
+  background-color: white;
+  border-radius: 5px;
+  position: relative;
+  box-sizing: border-box;
+  padding: 0.5rem;
+  color: black;
+  z-index: 2;
+  margin-bottom: 8px;
+
+  ${(props) =>
+    props.type === "send"
+      ? "border-radius: 0px 15px 15px 15px"
+      : "border-radius: 15px 0px 15px 15px"};
+
+  p {
+    text-align: right;
+  }
+`
+
+const StyledImg = styled.div`
+  height: 30px;
+  width: 100%;
+  display: flex;
+  margin-bottom: 8px;
+
+  ${(props) =>
+    props.type === "send"
+      ? "justify-content: flex-start"
+      : "justify-content: flex-end"}
+`
