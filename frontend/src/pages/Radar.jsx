04e6fd14ae0525b2de59e8451e4c70dd6a7493eb
catherineohrel <catherine_ohrel@hotmail.com>
2023-09-07 19:04:02 +0200

    Revert "commit before revert to last commit"
    This reverts commit 992da94b2b2a77410f59fb69fe6216bf1d09cbfd.

--- frontend/src/pages/Radar.jsx
+++ frontend/src/pages/Radar.jsx
@@ -1,50 +1,16 @@
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import RadarMap from "../components/radarMap"
 import "./styles/Radar.scss"
-// import mapLogo from "../assets/images/radarImg/mapLogo.png"
-// import { useState } from "react"
-
-// const [selectedOptions, setSelectedOptions] = useState("all")
-
-// const handleOptionChange = (selectedOptions) => {
-//   setSelectedOptions(selectedOptions)
-// }
 
 function Radar() {
   return (
-    <></>
-    // <div className="radar">
-    //   <section className="radar__title">
-    //     <img className="mapLogoImg" src={mapLogo} alt="logo map" />
-    //     <h3 className="mapTitle"> Map</h3>
-    //   </section>
-    //   <section className="radar__search"></section>
-    //   <section className="radar__map">
-    //     <RadarMap  center={[24.551, -77.786]} zoom={7} />
-    //   </section>
-    //   <section className="radar__filters">
-    //     <div className="mapFilter">
-    //       <select
-    //         name="target"
-    //         id="target"
-    //         onChange={(event) => handleOptionChange(event.target.value)}
-    //       >
-    //         <option value="all" className="radarFilterTitle">
-    //           Filters
-    //         </option>
-    //         <option value="Camping">Camping</option>
-    //         <option value="Shelter">Shelter</option>
-    //         <option value="Electricity">Electricity</option>
-    //         <option value="Oil & Gas">Oil & Gas</option>
-    //         <option value="Shop">Shop</option>
-    //         <option value="Water">Water</option>
-    //         <option value="Wood">Wood</option>
-    //         <option value="Hospital">Hospital</option>
-    //         <option value="Rebels">Rebels</option>
-    //         <option value="Zombies">Zombies</option>
-    //       </select>
-    //     </div>
-    //   </section>
-    // </div>
+    <div className="radar">
+      <section className="radar__title"></section>
+      <section className="radar__search"></section>
+      <section className="radar__map">
+        <RadarMap />
+      </section>
+      <section className="radar__filters"></section>
+    </div>
   )
 }
 
--- frontend/src/services/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL ?? "http://localhost:4242",
-})
-export default api
