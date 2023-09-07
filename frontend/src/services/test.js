782ef21a0aafb071d036bc57b7dc4c60b2a395ae
ANTOINE_NAME TUJAS_NAME <antoine.tujas33@gmail.com>
2023-09-07 23:37:58 +0200

    survivaladvice fini

--- frontend/src/services/test.js
+++ frontend/src/services/test.js
@@ -1,26 +1,26 @@
-// export const getOpenai = async (prompt) => {
-//   //   addPrompt("send", value)
+export const getOpenai = async (prompt) => {
+  const options = {
+    method: "POST",
+    body: JSON.stringify({
+      message: prompt,
+      id: 1,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
 
-//   const options = {
-//     method: "POST",
-//     body: JSON.stringify({
-//       message: prompt,
-//       id: 1,
-//     }),
-//     headers: {
-//       "Content-Type": "application/json",
-// //     },
-// //   }
+  try {
+    const response = await fetch("http://localhost:4242/openAPI", options)
+    const data = await response.json()
 
-// //   try {
-// //     const response = await fetch("http://localhost:4242/openAPI", options)
-// //     const data = await response.json()
-// //     // addPrompt("receive", data.choices[0].message.content)
-// //   } catch (error) {
-// //     console.error(error)
-// //   } finally {
-// //     // setValue(null)
-//   }
-
-//   return data.choices[0].message.content
-// }
+    // Ajoutez le prompt ici si nécessaire
+    // addPrompt("receive", data.choices[0].message.content)
+    console.log(data)
+    return data.choices[0].message.content
+  } catch (error) {
+    console.error(error)
+    // Gérez l'erreur ici si nécessaire
+    return null // Retournez une valeur par défaut ou null en cas d'erreur
+  }
+}
