e02b235edca9c27fd9014973b556e68d45678a7f
NayanN91 <nayan.nanouche@gmail.com>
2023-09-07 11:02:49 +0200

    widugettu done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,24 +11,28 @@ const widgetData = [
     widgetColor: "#7Bccc4",
     widgetName: "Survival zones",
     widgetImg: survivalzone,
+    linkedPage: "/zones",
   },
   {
     id: 2,
     widgetColor: "#CCEBC5",
     widgetName: "Survival Ressources",
     widgetImg: survivalressources,
+    linkedPage: "/ressources",
   },
   {
     id: 3,
     widgetColor: "#43A2CA",
     widgetName: "Chat",
     widgetImg: discussion,
+    linkedPage: "/discuss",
   },
   {
     id: 4,
     widgetColor: "#0868AC",
     widgetName: "Meet Survivors",
     widgetImg: meet,
+    linkedPage: "/meet",
   },
 ]
 
