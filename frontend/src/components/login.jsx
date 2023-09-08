a299b0fa3de1245969d9cbe34c7c03574460d76c
Ben-git01 <benbakirbenjamin@orange.fr>
2023-09-08 15:18:40 +0200

    scss tweaks and bug fixes

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,23 +1,65 @@
 import "./General/styles/login.scss"
 import LogoGoogle from "../assets/images/logoGoogle.png"
 import appleLogo from "../assets/images/apple.png"
-import { NavLink } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
 
 const Login = () => {
+  // State
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+  // HandleChange functions
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  // HandleSubmit function
+  const loginSubmit = () => {
+    const userData = { userName, password }
+    axios
+      .get("http://localhost:4242/", userData)
+      .then((response) => {
+        if (response.data !== undefined && response.data !== null) {
+          navigate("/home")
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   return (
     <div className="loginGlobal">
       <div>
         <h1 className="username">Username</h1>
-        <input className="username" type="text" name="" id="" />
+        <input
+          className="username"
+          type="text"
+          onChange={handleUserNameChange}
+        />
       </div>
       <div>
         <h2>Password</h2>
-        <input className="password" type="password" />
+        <input
+          className="password"
+          type="password"
+          onChange={handlePasswordChange}
+        />
       </div>
       <div>
-        <NavLink to="/home" exact activeClassName="active">
-          <button className="logButton">Log In</button>
-        </NavLink>
+        {/* <NavLink to="/home" exact activeClassName="active"> */}
+        <button className="logButton" onClick={loginSubmit}>
+          Log In
+        </button>
+        {/* </NavLink> */}
       </div>
       <div className="or">
         <hr />
