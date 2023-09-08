51786701a9a741fc76c434c3ee2e86e788ba8cbc
catherineohrel <catherine_ohrel@hotmail.com>
2023-09-08 10:45:53 +0200

    radar page done and fixed esLint errors in gptManager

--- backend/src/models/GptManager.js
+++ backend/src/models/GptManager.js
@@ -12,17 +12,17 @@ class GptManager extends AbstractManager {
   // insert(req, data) {
   // const { id, message } = req.body
   // const us erId = parseInt(id, 10)
-// console.log(id)
-// console.log(message)
-// console.log(data)
-// console.log(leMessage)
-// return this.database.query(
-//   `INSERT INTO ${this.table} (user_id, message_emis, message_recu) VALUES (1,"TEST","TEST2")`
-// )
-//   [
-//     (userId, message, data)
-//   ]
-// }
+  // console.log(id)
+  // console.log(message)
+  // console.log(data)
+  // console.log(leMessage)
+  // return this.database.query(
+  //   `INSERT INTO ${this.table} (user_id, message_emis, message_recu) VALUES (1,"TEST","TEST2")`
+  // )
+  //   [
+  //     (userId, message, data)
+  //   ]
+  // }
 
   update(cours) {
     return this.database.query(
