7820b7a2ce6f05f520b58b80046dd471a67f0da1
NayanN91 <nayan.nanouche@gmail.com>
2023-09-08 12:38:58 +0200

    logzer un peu de css

--- frontend/src/pages/log.jsx
+++ frontend/src/pages/log.jsx
@@ -4,6 +4,9 @@ import Login from "../components/login"
 function Log() {
   return (
     <div className="log">
+      <div className="register-title">
+        <h2>Log In</h2>
+      </div>
       <section className="log__section">
         <Login />
       </section>
